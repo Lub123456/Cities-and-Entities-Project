cc66872da2959180f59910adaecd5f5989386257
Lub123456 <93678601+Lub123456@users.noreply.github.com>
2025-06-14 16:54:44 +0300

    Update NamedEntity.java

--- src/NamedEntity.java
+++ src/NamedEntity.java
@@ -1,2 +1,16 @@
-public class NamedEntity {
+
+class NamedEntity {
+    int cityRef;
+    String nom, gen, acc, dat, gender, entityType;
+
+    public NamedEntity(String line) {
+        String[] parts = line.split("#");
+        this.nom = parts[0];
+        this.gen = parts[1];
+        this.acc = parts[2];
+        this.dat = parts[3];
+        this.gender = parts[4];
+        this.cityRef = Integer.parseInt(parts[5]);
+        this.entityType = parts[6];
+    }
 }
